42a69702a1853507b53f0c0a8b63c6af2892217e
oliinyk-alina-ip52 <oleynik.a.o@gmail.com>
2016-03-20 20:43:27 +0200

    documentation

--- 7.1/7.1.cpp
+++ 7.1/7.1.cpp
@@ -1,12 +1,44 @@
 #include<iostream>
-
+#include<string.h>
+#include<stdio.h>
 
 using namespace std;
 
-
+const int n = 100;
+char s[n];
+char a[n];
+char seps[] = " ,.;";
+void word(char *, char *);
 
 int main()
 {
-	int n;
+	printf("enter string with words: ");
+	gets_s(s);
+	printf("enter string with symbols: ");
+	gets_s(a);
+	word(s, a);
 	system("pause");
+}
+
+void word(char *p, char *q)
+{
+	char *tok;
+	char *t;
+	char *next = NULL;
+	int len = 0;
+	int k = 0;
+	tok = strtok_s(s, seps, &next);
+	t = strtok_s(a, seps, &next);
+	while (tok != NULL)
+	{
+		len = strlen(tok);
+		while (t != NULL)
+		{
+			k = strlen(t);
+			if (tok[0] == t[0] && tok[len - 1] == t[0])
+				printf("%c\n", tok);
+			t = strtok_s(NULL, seps, &next);
+		}
+		tok = strtok_s(NULL, seps, &next);
+	}
 }
